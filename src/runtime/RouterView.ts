7d2cae394e48c29b8feafae61e0ce1e97f727f47
FraCipolla <matcip@hotmail.com>
2025-11-26 13:51:12 +0100

    fixing routes not updating

--- src/runtime/RouterView.ts
+++ src/runtime/RouterView.ts
@@ -9,31 +9,35 @@ export interface RouteEntry {
 
 export const currentRoute = reactive(window.location.hash || "#/");
 
+window.addEventListener("hashchange", () => {
+  currentRoute.value = window.location.hash || "#/";
+});
+
 export function RouterView(routes: Record<string, RouteEntry>) {
   console.log(routes)
   const container = document.createElement("div");
-  effect(async () => {
-    console.log(currentRoute.value)
+  async function runRoute() {
+    console.log(currentRoute.value);
     container.innerHTML = "";
+
     const routeEntry = routes[currentRoute.value];
     if (!routeEntry) {
       container.textContent = "404 Not Found";
       return;
     }
 
-    // Load page
     let node = (await routeEntry.page()).default;
 
-    // Apply layouts if any
     if (routeEntry.layouts) {
       for (const layoutImport of routeEntry.layouts) {
         const layoutModule = await layoutImport();
-        node = layoutModule.default(node); // wrap the page node
+        node = layoutModule.default(node);
       }
     }
 
     container.appendChild(node);
-  });
+  }
+  effect(() => runRoute());
 
   return container;
 }
